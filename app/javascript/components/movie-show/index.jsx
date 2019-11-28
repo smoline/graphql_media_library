723dfd7b0019e43fe71da583aede3443938290be
Sherilyn Moline <smoline@gmail.com>
2019-11-28 13:37:17 -0500

    movie-show view query working

--- app/javascript/components/movie-show/index.jsx
+++ app/javascript/components/movie-show/index.jsx
@@ -1,45 +1,60 @@
 import React from 'react'
 import { Query } from 'react-apollo'
-import { MovieShow } from './movie-show.graphql'
+import { MovieShowQuery } from './movie-show.graphql'
 import Container from '@material-ui/core/Container'
 import { Rating } from '@material-ui/lab'
 import StarBorderIcon from '@material-ui/icons/StarBorder'
 import './movie.scss'
 
 export default class MovieShow extends React.Component {
+  fetchMovie = (id) => (
+    <Query query = { MovieShowQuery } variables = {{ id }}>
+      {({ loading, error, data }) => {
+        if (loading) return (<div>loading...</div>)
+        if (error) return `Error! ${error}`
+
+        return (
+          <div className="main-container">
+            <div className="movie-container">
+              <div className="image-box"><img src={data.movie.movieImageUrl} /></div>
+              <div className="text-container">
+                <div className="movie-title">
+                  <h3>{data.movie.title}</h3>
+                </div>
+                <div className="movie-row-rating">
+                  <Rating
+                    name="customized-empty"
+                    value={3}
+                    emptyIcon={<StarBorderIcon fontSize="inherit" color="primary" />}
+                  />
+                </div>
+                <div className="movie-row">
+                  <p>{data.movie.description}</p>
+                </div>
+                <div className="movie-row">
+                  <p>"...{data.movie.tagline}"</p>
+                </div>
+                <div className="movie-row">
+                  <p>Release Date: {data.movie.releaseDate}</p>
+                </div>
+                <div className="movie-row">
+                  <p>Runtime: {data.movie.runtime} mins</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      }}
+    </Query>
+  )
+
   render() {
     return (
       <Container maxWidth="xl">
         <header className="movie-header">
           <h2>Movie Show</h2>
         </header>
-        <Query query={MovieShow}>
-          {({ data, loading }) => (
-            <div className="main-container">
-              {loading ? 'loading...' : data.movies.map(({ id, title, description, tagline, tmdbId, imdbId, movieImageUrl, releaseDate, runtime }) => (
-                <div key={id} className="media-container">
-                  <div className="image-box"><img src={movieImageUrl} /></div>
-                  <div className="text-container">
-                    <div className="media-title">
-                      <p>{title}</p>
-                    </div>
-                    <div className="media-row3">
-                      <Rating
-                        name="customized-empty"
-                        value={3}
-                        emptyIcon={<StarBorderIcon fontSize="inherit" color="primary" />}
-                      />
-                    </div>
-                    <div className="media-row2">
-                      <p>{releaseDate}</p>
-                      <p>{runtime} mins</p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          )}
-        </Query>
+        {this.fetchMovie(this.props.match.params.id)}
       </Container>
     )
   }
