aaf4f2b56ffd0c9af735468ce75377cf1b8d1bc3
Sherilyn Moline <smoline@gmail.com>
2019-11-28 13:37:51 -0500

    movie-create started

---
+++ app/javascript/components/movie-create/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class MovieCreate extends React.Component {
+  render() {
+    return (
+      <Container maxWidth="xl">
+        <header className="movie-header">
+          <h2>Add a Movie</h2>
+        </header>
+        <div className="main-container">
+          
+        </div>
+      </Container>
+    )
+  }
+}
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom"
 import SearchAppBar from '../components/Navigation'
 import Movies from "../components/movies-index"
 import MovieShow from "../components/movie-show"
+import MovieCreate from "../components/movie-create"
 
 export default (
   <div>
@@ -11,6 +12,7 @@ export default (
       <Route exact path="/" component={Movies} />
       <Route exact path="/movies" component={Movies} />
       <Route exact path="/movies/:id" component={MovieShow} />
+      <Route exact path="/movies/create" component={MovieCreate} />
     </Switch>
   </div>
 )
