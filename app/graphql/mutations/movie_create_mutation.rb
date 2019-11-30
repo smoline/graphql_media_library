3b39670a25e3b6161a52dbb5a796a321fb81e049
Sherilyn Moline <smoline@gmail.com>
2019-11-30 11:10:35 -0500

    drying up inputs and authentication

--- app/graphql/mutations/movie_create_mutation.rb
+++ app/graphql/mutations/movie_create_mutation.rb
@@ -1,34 +1,14 @@
 module Mutations
   class MovieCreateMutation < Mutations::BaseMutation
-    argument :arguments, Inputs::MovieCreateInput, required: true
+    argument :attributes, Inputs::MovieCreateInput, required: true
 
     field :movie, Types::MovieType, null: false
     field :errors, [String], null: false
 
-    def resolve(title: arguments.title,
-                description: arguments.description,
-                tmdb_id: arguments.tmdb_id,
-                imdb_id: arguments.imdb_id,
-                release_date: arguments.release_date,
-                runtime: arguments.runtime,
-                tagline: arguments.tagline,
-                movie_image_url: arguments.movie_image_url)
-      if context[:current_user].nil?
-        raise GraphQL::ExecutionError,
-              "You need to authenticate to perform this action"
-      end
+    def resolve(attributes:)
+      check_authentication!
 
-      movie = Movie.new(
-        title: title,
-        description: description,
-        tmdb_id: tmdb_id,
-        imdb_id: imdb_id,
-        release_date: release_date,
-        runtime: runtime,
-        tagline: tagline,
-        movie_image_url: movie_image_url,
-        user: context[:current_user]
-      )
+      movie = Movie.new(attributes.to_h.merge(user: context[:current_user]))
 
       if movie.save
         { movie: movie }
--- app/graphql/mutations/movie_update_mutation.rb
+++ app/graphql/mutations/movie_update_mutation.rb
@@ -1,36 +1,17 @@
 module Mutations
   class MovieUpdateMutation < Mutations::BaseMutation
     argument :id, ID, required: true
-    argument :arguments, Inputs::MovieUpdateInput, required: true
+    argument :attributes, Inputs::MovieCreateInput, required: true
 
     field :movie, Types::MovieType, null: false
     field :errors, [String], null: false
 
-    def resolve(title: arguments.title,
-                description: arguments.description,
-                tmdb_id: arguments.tmdb_id,
-                imdb_id: arguments.imdb_id,
-                release_date: arguments.release_date,
-                runtime: arguments.runtime,
-                tagline: arguments.tagline,
-                movie_image_url: arguments.movie_image_url)
-      if context[:current_user].nil?
-        raise GraphQL::ExecutionError,
-              "You need to authenticate to perform this action"
-      end
-
+    def resolve(id:, attributes:)
+      check_authentication!
+      
       movie = Movie.find(id)
 
-      if movie.update(
-        title: title,
-        description: description,
-        tmdb_id: tmdb_id,
-        imdb_id: imdb_id,
-        release_date: release_date,
-        runtime: runtime,
-        tagline: tagline,
-        movie_image_url: movie_image_url
-      )
+      if movie.update(attributes.to_h)
         { movie: movie }
       else
         { errors: movie.errors.full_messages }
