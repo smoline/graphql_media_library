3d9783998346cd7fe38709c45e0280b1b9bc0855
Sherilyn Moline <smoline@gmail.com>
2019-12-04 12:35:00 -0500

    Functional Movie Form added

--- app/javascript/components/movie-form/index.jsx
+++ app/javascript/components/movie-form/index.jsx
@@ -1,63 +1,119 @@
-import React from 'react'
-import { FormControl, TextField } from '@material-ui/core'
+import React, { useState } from 'react'
+import Container from '@material-ui/core/Container'
+import { FormControl, TextField, Button } from '@material-ui/core'
 
-export default class MovieForm extends React.Component {
-  
-  render() {
-    return (
-      <div className="main-container">
-        <FormControl>
-          <TextField
-            label="Title"
-            id="outlined-basic"
-            label="Movie Title"
+const MovieForm = ({
+  initialTitle = '',
+  initialDescription = '',
+  initialReleaseDate = '',
+  initialRuntime = '',
+  initialTagline = '',
+  initialTmdbId = '',
+  initialImdbId = '',
+  initialMovieImageUrl = '',
+  onProcessMovie,
+  buttonText,
+  loading,
+}) => {
+  const [title, setTitle] = useState(initialTitle)
+  const [description, setDescription] = useState(initialDescription)
+  const [releaseDate, setReleaseDate] = useState(initialReleaseDate)
+  const [runtime, setRuntime] = useState(initialRuntime)
+  const [tagline, setTagline] = useState(initialTagline)
+  const [tmdbId, setTmdbId] = useState(initialTmdbId)
+  const [imdbId, setImdbId] = useState(initialImdbId)
+  const [movieImageUrl, setMovieImageUrl] = useState(initialMovieImageUrl)
+  return (
+    <Container maxWidth="xl">
+      <header className="movie-header">
+        <h2>Add a Movie</h2>
+      </header>
+      <FormControl>
+        <TextField
+          label="Title"
+          id="outlined-basic"
+          label="Movie Title"
+          variant="outlined"
+          margin="normal"
+          value={title}
+          onChange={e => setTitle(e.currentTarget.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Description"
+          variant="outlined"
+          margin="normal"
+          value={description}
+          onChange={e => setDescription(e.currentTarget.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Release Date"
+          variant="outlined"
+          margin="normal"
+          value={releaseDate}
+          onChange={e => setReleaseDate(e.currentTarget.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Runtime"
+          variant="outlined"
+          margin="normal"
+          value={runtime}
+          onChange={e => setRuntime(e.currentTarget.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Tagline"
+          variant="outlined"
+          margin="normal"
+          value={tagline}
+          onChange={e => setTagline(e.currentTarget.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="TMDB ID"
+          variant="outlined"
+          margin="normal"
+          value={tmdbId}
+          onChange={e => setTmdbId(e.currentTarget.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="IMDB ID"
+          variant="outlined"
+          margin="normal"
+          value={imdbId}
+          onChange={e => setImdbId(e.currentTarget.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Movie Image URL"
+          variant="outlined"
+          margin="normal"
+          value={movieImageUrl}
+          onChange={e => setMovieImageUrl(e.currentTarget.value)}
+        />
+        {loading ? (
+          '...Loading'
+        ) : (
+          <Button
             variant="outlined"
-            margin="normal"
-          />
-          <TextField
-            id="outlined-basic"
-            label="Description"
-            variant="outlined"
-            margin="normal"
-          />
-          <TextField
-            id="outlined-basic"
-            label="Release Date"
-            variant="outlined"
-            margin="normal"
-          />
-          <TextField
-            id="outlined-basic"
-            label="Runtime"
-            variant="outlined"
-            margin="normal"
-          />
-          <TextField
-            id="outlined-basic"
-            label="Tagline"
-            variant="outlined"
-            margin="normal"
-          />
-          <TextField
-            id="outlined-basic"
-            label="TMDB ID"
-            variant="outlined"
-            margin="normal"
-          />
-          <TextField
-            id="outlined-basic"
-            label="IMDB ID"
-            variant="outlined"
-            margin="normal"
-          />
-          <TextField
-            id="outlined-basic"
-            label="Movie Image URL"
-            variant="outlined"
-            margin="normal"
-          />
-        </FormControl>
-      </div>
-    )
-  }
+              onClick={() => onProcessMovie({
+                title,
+                description,
+                releaseDate,
+                runtime,
+                tagline,
+                tmdbId,
+                imdbId,
+                movieImageUrl })}>
+            {buttonText}
+          </Button>
+        )}
+      </FormControl>
+    </Container>
+  )
 }
+
+export default MovieForm
